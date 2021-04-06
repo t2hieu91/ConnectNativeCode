32283eb725d9f8f828d6e6bcd3644f772f0078c0
Trung Hieu <hieutt@d-soft.com.vn>
2021-04-06 15:07:14 +0700

    Native ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,26 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import <Flutter/Flutter.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    FlutterViewController * controller = (FlutterViewController *)self.window.rootViewController;
+    FlutterMethodChannel * batteryChannel = [FlutterMethodChannel methodChannelWithName:@"com.example.ConnectNativeCode/battery" binaryMessenger: controller.binaryMessenger];
+    
+    [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+      // Note: this method is invoked on the UI thread.
+      if ([@"getBatteryLevel" isEqualToString:call.method]) {
+          
+          result(@"Native code ios dayyyy.");
+            
+      } else {
+        result(FlutterMethodNotImplemented);
+      }
+    }];
+    
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
